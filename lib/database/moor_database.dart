3c9dbf22cfeb05664f6966b134fa9833c7e50ad2
zsimic1 <zvonimir_simic@hotmail.com>
2021-05-09 02:28:27 +0200

    Aplikacija napravljena preko StreamBuilder-a

--- lib/database/moor_database.dart
+++ lib/database/moor_database.dart
@@ -10,15 +10,34 @@ class Birds extends Table {
 
 @UseMoor(tables: [Birds])
 class AppDatabase extends _$AppDatabase {
-  AppDatabase()
+  AppDatabase._()
       : super(FlutterQueryExecutor.inDatabaseFolder(
-      path: 'db.sqlite', logStatements: true));
+            path: 'db.sqlite', logStatements: true));
   @override
   int get schemaVersion => 1;
 
+  static final AppDatabase _instance = AppDatabase._();
+  factory AppDatabase() {
+    return _instance;
+  }
 
   Future createBirdStatus(Bird bird) => into(birds).insert(bird);
-  Future updateBirdStatus(Bird bird) => update(birds).replace(bird);
-  Stream<List<Bird>> watchAllBirdStatuses() => select(birds).watch();
 
+  Future<Bird> getBird() =>
+      (select(birds)..where((bird) => bird.id.equals(0))).getSingle();
+  Future resetBirdStatus() {
+    var _i = 0;
+    var color = "white";
+    return update(birds).replace(Bird(id: 0, color: color, counter: _i));
+  }
+
+  Future updateBirdStatus(String color) async {
+    var _birdInDb = await getBird();
+    var _i = _birdInDb.counter;
+    _i++;
+    var _tempBird = _birdInDb.copyWith(color: color, counter: _i);
+    return update(birds).replace(_tempBird);
+  }
+
+  Stream<List<Bird>> watchAllBirdStatuses() => select(birds).watch();
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:birds/database/moor_database.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
+import 'database/moor_database.dart';
 
 void main() => runApp(MaterialApp(home: Birds()));
 
@@ -10,27 +11,29 @@ class Birds extends StatefulWidget {
   _BirdsState createState() => _BirdsState();
 }
 
-int i = 0;
-Color returnColor (String color)
-{
-  if(color=='blue')
-    {
-      return Colors.blue;
-    }
-  if(color=='red')
-  {
+Color returnColor(String color) {
+  if (color == 'blue') {
+    return Colors.blue;
+  }
+  if (color == 'red') {
     return Colors.red;
   }
-  if(color=='green')
-  {
+  if (color == 'green') {
     return Colors.green;
   }
-  if(color=='yellow')
-  {
+  if (color == 'yellow') {
     return Colors.yellow;
+  } else {
+    return Colors.white;
   }
 }
+
 class _BirdsState extends State<Birds> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -40,88 +43,85 @@ class _BirdsState extends State<Birds> {
         centerTitle: true,
         elevation: 0,
       ),
-      body: StreamBuilder(
-        stream: AppDatabase().watchAllBirdStatuses(),
-        builder: (context, AsyncSnapshot<List<Bird>> snapshot)
-          {
-            if(!snapshot.hasData){
-                i=0;
-                AppDatabase().createBirdStatus(Bird(color: Colors.white.toString(), counter: 0));
-            }
-            return Padding(
-              padding: EdgeInsets.all(5.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  TextField(
-                    textAlign: TextAlign.center,
-                    decoration: InputDecoration(
-                        labelText: "1",enabled:false,fillColor: returnColor(snapshot.data[0].color) , filled: true),
-                  ),
-                  Row(
+      body: Column(
+        children: [
+          StreamBuilder(
+              stream: AppDatabase().watchAllBirdStatuses(),
+              builder: (context, AsyncSnapshot<List<Bird>> snapshot) {
+                var _i;
+                var _color;
+                if (snapshot.hasData) {
+                  _i = snapshot.data[0].counter;
+                  _color = snapshot.data[0].color;
+                } else {
+                  _i = 0;
+                  _color = "white";
+                  AppDatabase().createBirdStatus(
+                      Bird(id: 0, color: _color, counter: _i));
+                }
+                return Padding(
+                  padding: EdgeInsets.all(5.0),
+                  child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      ElevatedButton(
-                          onPressed: () {
-                            setState(() {
-                              i++;
-                              AppDatabase().updateBirdStatus(Bird(id: 0, color:'red', counter: i ));
-                            });
-                          },
-                          style: ButtonStyle(
-                              backgroundColor:
-                              MaterialStateProperty.all<Color>(Colors.red)),
-                          child: Text('Bird 1')),
-                      SizedBox(width: 10),
-                      ElevatedButton(
-                          onPressed: () {
-                            setState(() {
-                              i++;
-                              AppDatabase().updateBirdStatus(Bird(id: 0, color:'blue', counter: i ));
-                            });
-                          },
-                          style: ButtonStyle(
-                              backgroundColor:
-                              MaterialStateProperty.all<Color>(Colors.blue)),
-                          child: Text('Bird 2')),
-                      SizedBox(width: 10),
-                      ElevatedButton(
-                          onPressed: () {
-                            setState(() {
-                              i++;
-                              AppDatabase().updateBirdStatus(Bird(id: 0, color:'green', counter: i ));
-                            });
-                          },
-                          style: ButtonStyle(
-                              backgroundColor:
-                              MaterialStateProperty.all<Color>(Colors.green)),
-                          child: Text('Bird 3')),
-                      SizedBox(width: 10),
-                      ElevatedButton(
-                          onPressed: () {
-                            setState(() {
-                              i++;
-                              AppDatabase().updateBirdStatus(Bird(id: 0, color:'yellow', counter: i ));
-                            });
-                          },
-                          style: ButtonStyle(
-                              backgroundColor:
-                              MaterialStateProperty.all<Color>(Colors.yellow)),
-                          child: Text('Bird 4')),
+                      Container(
+                        color: returnColor(_color),
+                        child: Row(
+                          mainAxisSize: MainAxisSize.max,
+                          children: [
+                            Text(_i.toString()),
+                          ],
+                        ),
+                      ),
                     ],
-                  )
-                ],
-              ),
-            );
-          }
+                  ),
+                );
+              }),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                  onPressed: () async {
+                    await AppDatabase().updateBirdStatus('red');
+                  },
+                  style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(Colors.red)),
+                  child: Text('Bird 1')),
+              SizedBox(width: 10),
+              ElevatedButton(
+                  onPressed: () async {
+                    await AppDatabase().updateBirdStatus('blue');
+                  },
+                  style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(Colors.blue)),
+                  child: Text('Bird 2')),
+              SizedBox(width: 10),
+              ElevatedButton(
+                  onPressed: () async {
+                    await AppDatabase().updateBirdStatus('green');
+                  },
+                  style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(Colors.green)),
+                  child: Text('Bird 3')),
+              SizedBox(width: 10),
+              ElevatedButton(
+                  onPressed: () async {
+                    await AppDatabase().updateBirdStatus('yellow');
+                  },
+                  style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(Colors.yellow)),
+                  child: Text('Bird 4')),
+            ],
+          )
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          setState(() {
-            i=0;
-            AppDatabase().updateBirdStatus(Bird(id: 0, color:'white', counter: i ));
-          });
-
+          AppDatabase().resetBirdStatus();
         },
         child: Text('Reset'),
       ),
